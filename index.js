abf732acf6b535f90b7add8d2358ee6722969bd0
Dmitriy Shishkin <6ast3r@gmail.com>
2025-12-24 18:16:07 +0700

    Copy to clipboard

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
+const {
+  exec,
+} = require('child_process');
+
 const input = `
-@react-navigation/bottom-tabs --------------- ◉ 7.5.0 -------- ◯ 7.7.1
-@react-navigation/elements ------------------ ◉ 2.7.0 -------- ◯ 2.8.0
-@react-navigation/material-top-tabs --------- ◉ 7.3.9 -------- ◯ 7.4.1
-@react-navigation/native -------------------- ◉ 7.1.18 ------- ◯ 7.1.19
-@react-navigation/stack --------------------- ◉ 7.5.0 -------- ◯ 7.6.1
-@shopify/flash-list ------------------------- ◉ 2.1.0 -------- ◯ 2.2.0
-@shopify/react-native-skia ------------------ ◉ 2.3.6 -------- ◯ 2.3.7
-expo ---------------------------------------- ◉ 54.0.20 ------ ◯ 54.0.21
-libphonenumber-js --------------------------- ◉ 1.12.24 ------ ◯ 1.12.25
-react-native-localize ----------------------- ◉ 3.5.4 -------- ◯ 3.6.0
-react-native-nitro-modules ------------------ ◉ 0.31.1 ------- ◯ 0.31.3
-react-native-qrcode-svg --------------------- ◉ 6.3.15 ------- ◯ 6.3.16
-react-native-safe-area-context -------------- ◉ 5.6.1 -------- ◯ 5.6.2
+@amplitude/analytics-react-native ----------- ◉ 1.5.16 ------- ◯ 1.5.31
+@legendapp/list ----------------------------- ◉ 2.0.18 ------- ◯ 2.0.19
+@shopify/react-native-skia ------------------ ◉ 2.4.7 -------- ◯ 2.4.14
+libphonenumber-js --------------------------- ◉ 1.12.31 ------ ◯ 1.12.33
+react-native-gesture-handler ---------------- ◉ 2.29.1 ------- ◯ 2.30.0
 `;
 
 const formatted = input.trim().split('\n').map(line => {
@@ -20,3 +16,11 @@ const formatted = input.trim().split('\n').map(line => {
 }).join('\n');
 
 console.log(formatted);
+
+exec(`echo "${formatted}" | pbcopy`, (exception) => {
+  if (exception != null) {
+    console.error(exception);
+  } else {
+    console.log('\n✓ Copied to clipboard');
+  }
+});
